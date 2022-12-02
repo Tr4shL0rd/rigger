56905ebef8b67928cb18ee577cfa2f5a9cad5941
Tr4shL0rd <ketil04@gmail.com>
2022-12-02 10:31:02 +0100

    removed prints

--- autoRig.py
+++ autoRig.py
@@ -1,9 +1,8 @@
+from tqdm import tqdm
 import time
 from selenium import webdriver
 
 def main(rigRound:int=0, rigMax:int=0):
-    print(f"doing {rigMax} rounds")
-    print(f"round: {rigRound}")
     polls = {
             "poll1": '//*[@id="option-05ZdRzJQVn6"]',
             "poll2": '//*[@id="option-eNg6Dv4deyA"]',
@@ -18,21 +17,23 @@ def main(rigRound:int=0, rigMax:int=0):
     driver = webdriver.Firefox()
     #school_url = "https://strawpoll.com/polls/kogjv3NP3g6/"
     url = "https://strawpoll.com/polls/61gDB8rvRyw"
-    print(f"going to {url}")
+    #print(f"going to {url}")
     driver.get(url)
-    print("finding consent button")
+    #print("finding consent button")
     time.sleep(1)
     driver.find_element_by_class_name("fc-button-label").click()
-    print("clicking poll option 1")
+    #print(f"clicking poll {polls['poll1'].key}")
     time.sleep(1)
     driver.find_element_by_xpath(polls["poll1"]).click()
-    print("finding vote button")
+    #print("finding vote button")
     time.sleep(1)
     driver.find_element_by_xpath("/html/body/div[1]/div[1]/div[2]/div[1]/form/div[8]/div/div[2]/button").click()
 
 try:
-    rigMax = 2
-    for i in range(rigMax):
+    rigMax = 1
+    base_time = 8
+    print(f"doing {rigMax} rounds")
+    for i in tqdm(range(rigMax)):
         main(i,rigMax)
         driver.close()
 except Exception as e:
